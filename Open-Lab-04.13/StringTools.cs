820937b9a7a9aa695e38437afd47277a2c89785f
IQcrew <itiqcrew@gmail.com>
2021-11-22 13:30:33 +0100

    hotovo

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -6,7 +6,28 @@ namespace Open_Lab_04._13
     {
         public string GetLongestCommonSequence(string str1, string str2)
         {
-            throw new NotImplementedException();
+            string temp_string = "";
+            string biggest_string = "";
+            for (int i = 0; i < str1.Length; i++)
+            {
+                temp_string = "";
+                for (int x = i; x < str1.Length; x++)
+                {
+                    temp_string += Convert.ToString(str1[x]);
+                    if (str2.Contains(temp_string))
+                    {
+                        if (temp_string.Length > biggest_string.Length)
+                        {
+                            biggest_string = temp_string;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return biggest_string;
         }
     }
 }
